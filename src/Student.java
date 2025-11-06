f4e9b31a54d4cb9a6b9eb911961ceed2bf4f05d7
KOCTUK1 <koctuk@rollins.edu>
2025-11-06 11:09:39 -0500

    Created Student Class
    class containing student information

---
+++ src/Student.java
@@ -0,0 +1,12 @@
+
+public class Student {
+	private String Name;
+	public int ID;
+	public int year;
+	
+	public Student(String n, int id, int y) {
+		this.Name = n;
+		this.ID = id;
+		this.year = y;
+	}
+}
