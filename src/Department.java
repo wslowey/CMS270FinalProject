cfd6266fd6d3b77e12450450d44aa39b17a91278
KOCTUK1 <koctuk@rollins.edu>
2025-11-06 11:17:48 -0500

    Department and professor class's
    Class's containing department and professor information

---
+++ src/Department.java
@@ -0,0 +1,11 @@
+
+public class Department {
+	public String Name;
+	public String Refrence;
+	
+	public Department(String n,String r) {
+		this.Name = n;
+		this.Refrence = r;
+	}
+
+}
---
+++ src/Professor.java
@@ -0,0 +1,13 @@
+
+public class Professor {
+	private String Name;
+	public int ID;
+	public Department department;
+	
+	public Professor(String n, int id, Department d){
+		this.Name = n;
+		this.ID = id;
+		this.department = d;
+	}
+
+}
